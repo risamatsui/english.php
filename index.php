0f9923c62a14c91babcf3b6e30b246fe2f401e46
nabetaku_0125 <nabetaku.0125@gmail.com>
2020-03-19 18:30:14 +0900

    一覧表示

--- index.php
+++ index.php
@@ -1,94 +1,60 @@
 <?php
-
 // session_start();
 
 // // $user = $_SESSION['user'] ?? [];
 // $user = [];
 
 // // ファイルの読み込み
-// require_once('Models/Task.php');
-// require_once('function.php');
+   require_once('./dbconnect.php');
+   require_once('./models/model.php');
+   require_once('./function.php');
 
-// $task = new Task();
-
-// if (isset($_GET['title'])) {
-//     $title = $_GET['title'];
-//     $tasks = $task->findByTitle(["%$title%"]);
-// } else {
-//     $tasks = $task->getAll();
-// }
-
+   $post = new Post();
+   $posts = $post->getAll();
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Todoアプリ</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
 </head>
 
-<body>
+    <header>
+        
+
+    <!---拓也さん-->
     <div class="container-fulid">
         <div class="row">
-            <div class="col-12">
+            <div class="header">
                 <nav class="navbar navbar-dark bg-dark">
-                    <a href="index.php" class="navbar-brand">lets start!!</a>
-                    <ul class="nav nav-pills">
-                        <li class="nav-item">
-                            <!-- <span class="nav-link text-light">
-                                <?= 'ログインユーザーのメールアドレス' ?>
-                            </span> -->
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-light" href="create.php">Create</a>
-                        </li>
-                        <li class="nav-item">
-                            <?php if ($user === null) : ?>
-                                <a class="nav-link text-light" href="signinform.php">サインイン</a>
-                            <?php else : ?>
-                                <a class="nav-link text-light" href="signout.php">サインアウト</a>
-                            <?php endif; ?>
-                        </li>
-                        <li class="nav-item">
-                            <!-- <form action="" class="form-inline">
-                                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="title">
-                                <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
-                            </form> -->
-                        </li>
-                    </ul>
-
+                    <a href="./test.php" class="navbar-brand">lets test</a>
+                    <a href="./create.php" class="navbar-brand">create</a>
+                    <a href="./delete.php" class="navbar-brand">delete</a>
                 </nav>
             </div>
         </div>
 
-        <div class="row p-3">
-            <?php foreach ($tasks as $task) : ?>
-                <div class="col-sm-6 col-md-4 col-lg-3 py-3 py-3">
-                    <div class="card">
-                        <img src="https://picsum.photos/200" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title"><?= h($task["title"]); ?></h5>
-                            <p class="card-text">
-                                <?= h($task["contents"]); ?>
-                            </p>
-                            <div class="text-right d-flex justify-content-end">
-                                <a href="edit.php?id=<?= h($task['id']); ?>" class="btn text-success">EDIT</a>
-                                <form action="delete.php" method="post">
-                                    <input type="hidden" name="id" value="<?= h($task['id']); ?>">
-                                    <button type="submit" class="btn text-danger">DELETE</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
+    </div>
+    <!----->
+<link rel="stylesheet" href="./style.css">
+    </header>
+
+<body>   
+    <?php foreach ($posts as $post) : ?>
+    <div class="fruit">
+        <div class="one">
+            <input type="checkbox" name=“sample” value="1">
+                <div class="question">
+                   <?= h($post["meaning"]); ?>
+                </div> 
+                <div class="answer">
+                   <?= h($post["words"]); ?>
                 </div>
-            <?php endforeach; ?>
         </div>
     </div>
-
-
+    <?php endforeach; ?>
 </body>
-
 </html>
